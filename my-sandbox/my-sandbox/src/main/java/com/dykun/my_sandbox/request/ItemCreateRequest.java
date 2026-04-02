d8720409bf05a6e8c45dda936e349258ffcbec86
IgorDykun <150623897+IgorDykun@users.noreply.github.com>
2026-04-02 22:00:09 +0300

    Delete my-sandbox/my-sandbox directory

--- my-sandbox/my-sandbox/src/main/java/com/dykun/my_sandbox/request/ItemCreateRequest.java
+++
@@ -1,8 +0,0 @@
-package com.dykun.my_sandbox.request;
-
-public class ItemCreateRequest {
-    private String name;
-    
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-}
